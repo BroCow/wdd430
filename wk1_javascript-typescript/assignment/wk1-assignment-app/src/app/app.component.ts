63c8412c129e9aed0d742629a4f386e72abe64c7
Christopher Cowan <soundcommchris@gmail.com>
2022-01-06 18:46:47 -0800

    wk1 assignment

---
+++ wk1_javascript-typescript/assignment/wk1-assignment-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = `Chris Cowan's wk1-assignment-app`;
+  name = `Enter name!`;
+}
